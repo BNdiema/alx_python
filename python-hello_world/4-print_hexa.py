87fdfa2f502b00e93e70ff74effdde814183b381
Ndiemab <bryantndiema@protonmail.com>
2023-12-27 00:13:54 +0300

    program that prints all numbers from 0 to 98 in decimal and in hexadecimal

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -2,4 +2,4 @@
 
 #printing all numbers from 0 to 98 in decimal and hexadecimal
 for i in range (99):
-    print('{:d} {} {:x}' .format (i, '= 0 x', i))
+    print('{:d} {} {:x}' .format(i, '= 0 x', i))
