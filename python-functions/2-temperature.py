d11fcddb16940006d789531e9109dc684ce32a15
Ndiemab <bryantndiema@protonmail.com>
2024-01-08 00:58:22 +0300

    Python function called reverse_string that takes a string as input and returns the reverse of that string.

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,4 @@
-#!/sur/bin/python3
+#!/usr/bin/python3
 
 #Temperature Converter Function
 def convert_to_celsius(fahrenheit):
---
+++ python-functions/3-string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+#String Manipulation Functiom
+def reverse_string(string):
+    return(string[::-1])
